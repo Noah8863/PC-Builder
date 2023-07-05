87ec6d53fc6cd976977a875ec0386cd5256c0b95
Noah8863 <stacknoah99@gmail.com>
2023-07-05 17:31:31 -0600

    configured all files/folders

---
+++ client/src/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+      extend: {
+        fontSize: {
+          'xxs': '0.625rem',
+          'xs': '0.75rem',     
+          'sm': '0.875rem',    
+          'base': '1rem', 
+          'lg': '1.125rem', 
+          'xl': '1.25rem',
+          'xxl': '1.75rem',
+          '2xl': '2rem',
+          '3xl': '3.5rem',
+          'superText': '5rem',
+        },
+      },
+    },
+    plugins: [],
+  }
