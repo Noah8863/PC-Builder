07010780b2f28b2594e2c2f6e2d7354d6c7cfb42
Noah8863 <stacknoah99@gmail.com>
2023-07-05 23:09:12 -0600

    Moved package files and modified server.js files

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
+app.get('/parts', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
-const port = process.env.PORT || 3000;
+
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
