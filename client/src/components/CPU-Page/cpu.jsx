f128b48ccd9b6bf90538bb67e5b7c7bce16f1aa4
Noah8863 <stacknoah99@gmail.com>
2023-07-06 13:25:00 -0600

    got the cpu page to render all the JSON dynamically

--- client/src/components/CPU-Page/cpu.jsx
+++ client/src/components/CPU-Page/cpu.jsx
@@ -25,24 +25,47 @@ function CPU() {
 
 
   return (
-    <div>
-      <p>Hello! This is the CPU component!</p>
-      <ul>
-        {cpuParts.map((part) => (
-          <li key={part.id}>{part.size}</li>
-        ))}
-      </ul>
-      <div>
-        {cpuParts.map((part) => {
-          return <p key={part.id}>{part.color}</p>
-        })}
+    <main>
+      <div className="m-4 text-center text-2xl underline underline-offset-4 h-32 bg-slate-400">
+        MotherBoards
       </div>
-      <div>
-        {cpuParts.map((part) => {
-          return <p key={part.id}>{part.price}</p>
-        })}
+      <div className="flex">
+        <div className="w-1/5 bg-gray-200 max-h-screen m-4">
+          <p className="text-xxl text-center">Sort By:</p>
+        </div>
+
+        <div className="w-4/5 bg-gray-300 m-4">
+          <div className="bg-white">
+            <div className="grid grid-cols-4 gap-4">
+              {cpuParts.length > 0 &&
+                cpuParts.map((part, index) => (
+                  <div key={index} className="relative">
+                    <div className="flex flex-col items-center justify-center">
+                      <img
+                        src={part.img}
+                        alt={part.title}
+                        className="max-w-full h-auto"
+                      />
+                      <div className="absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-300">
+                        <div className="absolute bg-gray-800 text-white p-4 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                          <p className="text-center">
+                            Socket Type: {part.socket}
+                          </p>
+                          <p className="text-center">
+                            Clock Speeds: {part.clockSpeed}
+                          </p>
+                        </div>
+                      </div>
+                      <p>{part.manufacturer}</p>
+                      <p>{part.model}</p>
+                    </div>
+                  </div>
+                ))}
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </main>
   );
   
 }
