9395da05481ac248a48d1001636d4f0d173e2b64
Noah8863 <stacknoah99@gmail.com>
2023-07-06 18:00:43 -0600

    added hover message for more information on motherboards

--- client/src/components/Motherboard-Page/motherboard.jsx
+++ client/src/components/Motherboard-Page/motherboard.jsx
@@ -51,40 +51,104 @@ function Motherboards() {
 
         <div className="w-4/5 bg-gray-300 m-4">
           <div className="grid grid-cols-4 gap-4">
-            <div className="bg-white">
+            <div className="bg-white flex flex-col items-center justify-center">
               {parts.length > 0 && (
                 <>
-                  <img src={parts[0].img} alt={parts[0].title} />
+                  <div className="relative">
+                    <img
+                      src={parts[0].img}
+                      alt={parts[0].title}
+                      className="max-w-full h-auto"
+                    />
+                    <div className="absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-300">
+                      <div className="absolute bg-gray-800 text-white p-4 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                        <p className="text-center">
+                          Socket Type: {parts[0].socket}
+                        </p>
+                        <p className="text-center">
+                          Motherboard Size: {parts[0].size}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
                   <p>{parts[0].manufacturer}</p>
                   <p>{parts[0].model}</p>
                 </>
               )}
             </div>
 
-            <div className="bg-white">
-            {parts.length > 0 && (
+            <div className="bg-white flex flex-col items-center justify-center">
+              {parts.length > 0 && (
                 <>
-                  <img src={parts[1].img} alt={parts[1].title} />
+                  <div className="relative">
+                    <img
+                      src={parts[1].img}
+                      alt={parts[1].title}
+                      className="max-w-full h-auto"
+                    />
+                    <div className="absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-300">
+                      <div className="absolute bg-gray-800 text-white p-4 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                        <p className="text-center">
+                          Socket Type: {parts[1].socket}
+                        </p>
+                        <p className="text-center">
+                          Motherboard Size: {parts[1].size}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
                   <p>{parts[1].manufacturer}</p>
                   <p>{parts[1].model}</p>
                 </>
               )}
             </div>
 
-            <div className="bg-white">
-            {parts.length > 0 && (
+            <div className="bg-white flex flex-col items-center justify-center">
+              {parts.length > 0 && (
                 <>
-                  <img src={parts[2].img} alt={parts[2].title} />
+                  <div className="relative">
+                    <img
+                      src={parts[2].img}
+                      alt={parts[2].title}
+                      className="max-w-full h-auto"
+                    />
+                    <div className="absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-300">
+                      <div className="absolute bg-gray-800 text-white p-4 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                        <p className="text-center">
+                          Socket Type: {parts[2].socket}
+                        </p>
+                        <p className="text-center">
+                          Motherboard Size: {parts[2].size}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
                   <p>{parts[2].manufacturer}</p>
                   <p>{parts[2].model}</p>
                 </>
               )}
             </div>
 
-            <div className="bg-white">
-            {parts.length > 0 && (
+            <div className="bg-white flex flex-col items-center justify-center">
+              {parts.length > 0 && (
                 <>
-                  <img src={parts[3].img} alt={parts[3].title} />
+                  <div className="relative">
+                    <img
+                      src={parts[3].img}
+                      alt={parts[3].title}
+                      className="max-w-full h-auto"
+                    />
+                    <div className="absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-300">
+                      <div className="absolute bg-gray-800 text-white p-4 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                        <p className="text-center">
+                          Socket Type: {parts[3].socket}
+                        </p>
+                        <p className="text-center">
+                          Motherboard Size: {parts[3].size}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
                   <p>{parts[3].manufacturer}</p>
                   <p>{parts[3].model}</p>
                 </>
