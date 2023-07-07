22e3a8e7864130277bab558f48e2d4ee6d35329f
Noah8863 <stacknoah99@gmail.com>
2023-07-07 17:52:58 -0600

    moved algorthims over to CPU page

--- client/src/components/Motherboard-Page/motherboard.jsx
+++ client/src/components/Motherboard-Page/motherboard.jsx
@@ -21,38 +21,112 @@ function Motherboards() {
       });
   }, []);
 
+
+  const sortMax = () => {
+    const sortedParts = [...parts].sort((a, b) => b.price - a.price);
+    setParts(sortedParts);
+  };
+
+  const sortLeast = () => {
+    const sortedParts = [...parts].sort((a, b) => a.price - b.price);
+    setParts(sortedParts);
+  };
+
+  const sortAZ = () => {
+    const sortedParts = [...parts].sort((a, b) =>
+      a.manufacturer.localeCompare(b.manufacturer)
+    );
+    setParts(sortedParts);
+  };
+
+  const sortZA = () => {
+    const sortedParts = [...parts].sort((a, b) =>
+      b.manufacturer.localeCompare(a.manufacturer)
+    );
+    setParts(sortedParts);
+  };
+
+  const sortBlackFirst = () => {
+    const sortedParts = [...parts].sort((a, b) =>
+      a.color.localeCompare(b.color)
+    );
+    setParts(sortedParts);
+  };
+
+  const sortWhiteFirst = () => {
+    const sortedParts = [...parts].sort((a, b) =>
+      b.color.localeCompare(a.color)
+    );
+    setParts(sortedParts);
+  };
+
   return (
-    <main>
-      <div className="m-4 text-center text-2xl underline underline-offset-4 h-32 bg-slate-400">
+    <main className="z-1">
+      <div className="m-4 text-center text-2xl underline underline-offset-4 h-32 bg-slate-400 pt-10">
         MotherBoards
       </div>
-      <div className="flex">
-        <div className="w-1/5 bg-gray-200 max-h-screen m-4">
-          <p className="text-xxl text-center">Sort By:</p>
+      <div className="flex flex-col sm:flex-row ">
+        <div className="w-90 sm:w-1/5 bg-gray-200 base:min-h-screen m-4">
+          <p className="text-xxl text-left px-4 py-2">Sort By:</p>
+          <button
+            className="block px-4 py-2 text-lg text-gray-700 hover:bg-gray-100 hover:text-gray-900 w-full text-left"
+            onClick={sortMax}
+          >
+            Price: Highest to Lowest
+          </button>
+          <button
+            className="block px-4 py-2 text-lg text-gray-700 hover:bg-gray-100 hover:text-gray-900 w-full text-left"
+            onClick={sortLeast}
+          >
+            Price: Lowest to Highest
+          </button>
+          <button
+            className="block px-4 py-2 text-lg text-gray-700 hover:bg-gray-100 hover:text-gray-900 w-full text-left"
+            onClick={sortAZ}
+          >
+            Sort: A - Z
+          </button>
+          <button
+            className="block px-4 py-2 text-lg text-gray-700 hover:bg-gray-100 hover:text-gray-900 w-full text-left"
+            onClick={sortZA}
+          >
+            Sort: Z - A
+          </button>
+          <button
+            className="block px-4 py-2 text-lg text-gray-700 hover:bg-gray-100 hover:text-gray-900 w-full text-left"
+            onClick={sortBlackFirst}
+          >
+            Color: Black
+          </button>
+          <button
+            className="block px-4 py-2 text-lg text-gray-700 hover:bg-gray-100 hover:text-gray-900 w-full text-left"
+            onClick={sortWhiteFirst}
+          >
+            Color: White
+          </button>
         </div>
 
-        <div className="w-4/5 bg-gray-300 m-4">
+        <div className="w-full sm:w-4/5 bg-gray-300 m-4 z-1">
           <div className="bg-white">
-            <div className="grid grid-cols-4 gap-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
               {parts.length > 0 &&
                 parts.map((part, index) => (
                   <div key={index} className="relative">
-                    <div className="flex flex-col items-center justify-center">
+                    <div className="flex flex-col items-center justify-cent z-1">
                       <img
                         src={part.img}
                         alt={part.title}
-                        className="max-w-full h-auto"
+                        className="w-80 h-auto p-2 z-1"
                       />
                       <div className="absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-300">
                         <div className="absolute bg-gray-800 text-white p-4 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                          <p className="text-center">Color: {part.color}</p>
                           <p className="text-center">
                             Socket Type: {part.socket}
                           </p>
-                          <p className="text-center">
-                            Motherboard Size: {part.size}
-                          </p>
                         </div>
                       </div>
+                      <p>{part.price}</p>
                       <p>{part.manufacturer}</p>
                       <p>{part.model}</p>
                     </div>
