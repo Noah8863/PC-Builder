44a986b2570ed44cedd3990d01340848e03dbd1e
Noah8863 <stacknoah99@gmail.com>
2023-07-08 17:13:03 -0600

    most expensive/cheapest MB, CPU and GPU set up

--- client/src/components/Home/home.jsx
+++ client/src/components/Home/home.jsx
@@ -29,9 +29,81 @@ function Home() {
       });
   }, []);
 
+
+  const findMostExpensiveItem = (partCategory) => {
+    const filteredParts = allParts.filter((part) => partCategory.some((prefix) => part.id.startsWith(prefix)));
+    if (filteredParts.length > 0) {
+      const mostExpensiveItem = filteredParts.reduce((maxPriceItem, currentItem) => {
+        return currentItem.price > maxPriceItem.price ? currentItem : maxPriceItem;
+      });
+      return mostExpensiveItem;
+    }
+    return null;
+  };
+
+  const partCategories = {
+    Motherboard: ["MB"],
+    CPU: ["CPU"],
+    GPU: ["GPU"],
+    // Add more categories as needed
+  };
+
+  const findCheapestItem = (partCategory) => {
+    const filteredParts = allParts.filter((part) => partCategory.some((prefix) => part.id.startsWith(prefix)));
+    if (filteredParts.length > 0) {
+      const cheapestItem = filteredParts.reduce((minPriceItem, currentItem) => {
+        return currentItem.price < minPriceItem.price ? currentItem : minPriceItem;
+      });
+      return cheapestItem;
+    }
+    return null;
+  };
+
+  const partCategorie = {
+    Motherboard: ["MB"],
+    CPU: ["CPU"],
+    GPU: ["GPU"],
+    // Add more categories as needed
+  };
+
   return (
     <main>
-      {allParts && allParts.map((part) => <li key={part.id}>{part.price}</li>)}
+      <h2>Most Expensive Items</h2>
+      {Object.entries(partCategories).map(([category, prefixes]) => {
+        const mostExpensiveItem = findMostExpensiveItem(prefixes);
+        return (
+          <div key={category}>
+            <h3>{category}</h3>
+            {mostExpensiveItem ? (
+              <>
+                <img src={mostExpensiveItem.img} alt={mostExpensiveItem.title} className="w-80 h-auto p-2 z-1"/>
+                <p>Title: {mostExpensiveItem.title}</p>
+                <p>Price: {mostExpensiveItem.price}</p>
+              </>
+            ) : (
+              <p>No item found</p>
+            )}
+          </div>
+        );
+      })}
+      <h2>Cheapest Items</h2>
+      {Object.entries(partCategorie).map(([category, prefixes]) => {
+        const cheapestItem = findCheapestItem(prefixes);
+        return (
+          <div key={category}>
+            <h3>{category}</h3>
+            {cheapestItem ? (
+              <>
+                <img src={cheapestItem.img} alt={cheapestItem.title} className="w-80 h-auto p-2 z-1"/>
+                <p>Title: {cheapestItem.title}</p>
+                <p>Price: {cheapestItem.price}</p>
+              </>
+            ) : (
+              <p>No item found</p>
+            )}
+          </div>
+        );
+      })}
     </main>
   );
 }
