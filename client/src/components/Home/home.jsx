6c32c5ee2e139853f76adbeaf9eb4bb46a103541
Noah8863 <stacknoah99@gmail.com>
2023-07-08 16:55:11 -0600

    got the starting logic to return all the parts data

--- client/src/components/Home/home.jsx
+++ client/src/components/Home/home.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 function Home() {
+  const [allParts, setAllParts] = useState([]);
+
+  useEffect(() => {
+    fetch("/parts/")
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error("Error fetching parts data");
+        }
+      })
+      .then((jsonResult) => {
+        const partList = [
+          ...jsonResult.motherboards,
+          ...jsonResult.cpus,
+          ...jsonResult.gpus,
+          ...jsonResult.ram,
+          ...jsonResult.cases,
+          ...jsonResult.fans,
+          ...jsonResult.powerSupply,
+        ];
+        setAllParts(partList);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
-    <div>
-      <p>Hello! This is the Home component!</p>
-    </div>
+    <main>
+      {allParts && allParts.map((part) => <li key={part.id}>{part.price}</li>)}
+    </main>
   );
 }
 
