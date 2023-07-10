a18589030c14f8b4648c1b9b39d2975d83be7a61
Noah8863 <stacknoah99@gmail.com>
2023-07-10 15:08:44 -0600

    added shop by category section and styles sections a bit better

--- client/src/components/Home/home.jsx
+++ client/src/components/Home/home.jsx
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from "react";
+import CPUImage from "../../images/cpu.jpg";
+import GPUImage from "../../images/gpu.jpg";
+import MotherboardImage from "../../images/motherboard.png";
+import PowerSupplyImage from "../../images/psu.jpg";
+import FanImage from "../../images/fans.jpg";
+import RamImage from "../../images/ram.jpg";
+import CaseImage from "../../images/case.jpg";
 
 function Home() {
   const [allParts, setAllParts] = useState([]);
   const [dataLoaded, setDataLoaded] = useState(false);
-  
+
   useEffect(() => {
     fetch("/parts/")
       .then((res) => {
@@ -90,15 +97,15 @@ function Home() {
   }
 
   return (
-    <main>
-      <div className="">
-        <h2>Most Expensive Items</h2>
-        <div className="bg-blue-300 grid grid-cols-7">
+    <main className="w-5/6 m-auto justify-center bg-gray-400 h-contain">
+      <div>
+        <p className="text-2xl">Most Expensive Items</p>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-7 gap-3">
           {Object.entries(partCategories).map(([category, prefixes]) => {
             const mostExpensiveItem = findMostExpensiveItem(prefixes);
             return (
               <div key={category} className="bg-blue-300 grid">
-                <div className="grid">
+                <div className="grid p-6 bg-white m-4 items-center text-center text-xl justify-center">
                   <h3>{category}</h3>
                   <img
                     src={mostExpensiveItem.img}
@@ -113,25 +120,71 @@ function Home() {
           })}
         </div>
       </div>
-      <h2>Cheapest Items</h2>
-      <div className="bg-blue-300 grid grid-cols-7">
+      <p className="text-2xl">Cheapest Items</p>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-7 gap-3">
         {Object.entries(partCategorie).map(([category, prefixes]) => {
           const cheapestItem = findCheapestItem(prefixes);
           return (
-            <div key={category} className="bg-blue-300 grid">
-              <div className="grid">
+            <div
+              key={category}
+              className="grid p-6 bg-blue-300 m-4 items-center text-center text-xl justify-center rounded-lg p-4"
+            >
+              <div className="grid bg-white">
                 <h3>{category}</h3>
-                <img
-                  src={cheapestItem.img}
-                  alt={cheapestItem.title}
-                  className="w-60 max-h-60 p-2 z-1"
-                />
+                <div className="p-4">
+                  <img
+                    src={cheapestItem.img}
+                    alt={cheapestItem.title}
+                    className="max-w-60 max-h-60"
+                  />
+                </div>
                 <p>Title: {cheapestItem.title}</p>
                 <p>Price: {cheapestItem.price}</p>
               </div>
             </div>
           );
         })}
+      </div>
+      <div>
+        <p className="text-xxl text-center">Shop by Category</p>
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-4 m-4 p-4">
+          <button className="flex flex-col items-center justify-center bg-blue-500 text-white rounded-lg p-4">
+            <a href="/CPU"><img src={CPUImage} alt="CPU" className="max-w-40 max-h-40" /></a>
+            CPUs
+          </button>
+          <button className="flex flex-col items-center justify-center bg-blue-500 text-white rounded-lg p-4">
+          <a href="/GPU"><img src={GPUImage} alt="GPU" className="max-w-40 max-h-40" /></a>
+            GPUs
+          </button>
+          <button className="flex flex-col items-center justify-center bg-blue-500 text-white rounded-lg p-4">
+            <a href="/motherboards"><img
+              src={MotherboardImage}
+              alt="Motherboard"
+              className="max-w-40 max-h-40"
+            /></a>
+            Motherboards
+          </button>
+          <button className="flex flex-col items-center justify-center bg-blue-500 text-white rounded-lg p-4">
+            <a href="/RAM"><img src={RamImage} alt="RAM" className="max-w-40 max-h-40" /></a>
+            RAM
+          </button>
+          <button className="flex flex-col items-center justify-center bg-blue-500 text-white rounded-lg p-4">
+            <a href="/Fans"><img src={FanImage} alt="Fan" className="max-w-40 max-h-40" /></a>
+            Fans
+          </button>
+          <button className="flex flex-col items-center justify-center bg-blue-500 text-white rounded-lg p-4">
+            <a href="/PowerSupply"><img
+              src={PowerSupplyImage}
+              alt="Power Supply"
+              className="max-w-40 max-h-40"
+            /></a>
+            Power Supplies
+          </button>
+          <button className="flex flex-col items-center justify-center bg-blue-500 text-white rounded-lg p-4">
+            <a href="/Cases"><img src={CaseImage} alt="Case" className="max-w-40 max-h-40" /></a>
+            Cases
+          </button>
+        </div>
       </div>
     </main>
   );
