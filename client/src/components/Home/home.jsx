baff564012ab332239e956d4a0c548b4ae242e23
Noah8863 <stacknoah99@gmail.com>
2023-07-13 16:37:28 -0600

    started to make landing page info containers

--- client/src/components/Home/home.jsx
+++ client/src/components/Home/home.jsx
@@ -103,21 +103,38 @@ function Home() {
   return (
     <main className=" m-auto justify-center h-contain">
       {/* Section 1 */}
-      <header className="customBackground h-screen flex flex-col items-center justify-start pt-40">
-        <h1 className="text-5xl text-center mb-8 text-white">
-          Welcome to the PC-Builder Community!
-        </h1>
-        <div className="grid grid-cols-2 gap-8 w-3/4 mt-10">
-          <div className="bg-gray-200 p-10">
-            <h2 className="text-2xl mb-4">Left Column</h2>
-            <p>Placeholder text here</p>
-          </div>
-          <div className="bg-gray-200 p-4">
-            <h2 className="text-2xl mb-4">Right Column</h2>
-            <p>Placeholder text here</p>
-          </div>
-        </div>
-      </header>
+      <header className="customBackground h-screen flex flex-col items-center justify-start pt-10">
+  <h1 className="text-5xl text-center mb-8">Welcome to the PC-Builder Community!</h1>
+  <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 w-3/4 mt-10">
+    <div className="bg-gray-200 p-8 rounded-lg shadow-lg">
+      <h2 className="text-3xl mb-6">Build Your Dream PC</h2>
+      <p className="mb-4">
+        Are you looking to build your own custom PC? Our PC-Builder project offers a wide range of high-quality PC components to help you create the perfect system tailored to your needs.
+      </p>
+      <p className="mb-4">
+        From powerful CPUs and GPUs to reliable motherboards and memory modules, we have all the essential components you need to bring your dream PC to life.
+      </p>
+      <p className="mb-4">
+        Explore our extensive collection and start building your PC today!
+      </p>
+      <a href="/signup" className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-lg inline-block">Sign Up Now</a>
+    </div>
+    <div className="bg-gray-200 p-8 rounded-lg shadow-lg mt-8 sm:mt-0">
+      <h2 className="text-3xl mb-6">Join the PC-Builder Community</h2>
+      <p className="mb-4">
+        Building a PC is not just about the hardware; it's also about the community. Join our PC-Builder community and connect with fellow PC enthusiasts from around the world.
+      </p>
+      <p className="mb-4">
+        Share your experiences, ask questions, and learn from others. Our community blog is the perfect place to discuss PC building tips, troubleshoot issues, and stay up-to-date with the latest trends.
+      </p>
+      <p className="mb-4">
+        Whether you're a beginner or an experienced builder, our community is here to support you throughout your PC-building journey.
+      </p>
+      <a href="/signup" className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-lg inline-block">Join the Community</a>
+    </div>
+  </div>
+</header>
+
 
       {/* Section 2 */}
       <section className="w-5/6 justify-center m-auto">
