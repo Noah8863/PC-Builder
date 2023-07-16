109efff78faee83a0d33ec91d783adcbf29b87f1
Noah8863 <stacknoah99@gmail.com>
2023-07-16 10:00:07 -0600

    successfully had my first user signup

--- client/src/components/signup/signup.jsx
+++ client/src/components/signup/signup.jsx
@@ -1,9 +1,13 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import { auth } from "../../config/firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 function SignUp() {
-  const signIn = () => {};
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const signIn = async () => {
+    await createUserWithEmailAndPassword(auth, email, password);
+  };
 
   return (
     //Component below belongs to flowbite code snippet. See https://flowbite.com/blocks/marketing/register/ for more details
@@ -25,14 +29,12 @@ function SignUp() {
                 <label
                   htmlFor="email"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  required
+                 
                 >
                   Your email
                 </label>
                 <input
-                  type="email"
-                  name="email"
-                  id="email"
+                  onChange={(e) => setEmail(e.target.value)}
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="name@company.com"
                   required=""
@@ -47,12 +49,10 @@ function SignUp() {
                   Password
                 </label>
                 <input
-                  type="password"
-                  name="password"
-                  id="password"
+                  onChange={(e) => setPassword(e.target.value)}
                   placeholder="••••••••"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
+                 
                 />
               </div>
               <div>
@@ -73,6 +73,7 @@ function SignUp() {
               </div>
               <button
                 type="submit"
+                onClick={signIn}
                 className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
               >
                 Create an account
--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -1,16 +1,18 @@
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth"
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCNZVuK1AB2J9GARY92Tk_fib0lrflnuAQ",
-//   authDomain: "pc-builder-a804d.firebaseapp.com",
-//   projectId: "pc-builder-a804d",
-//   storageBucket: "pc-builder-a804d.appspot.com",
-//   messagingSenderId: "42029449411",
-//   appId: "1:42029449411:web:b12adc8178dbe8f4c62276",
-//   measurementId: "G-1NSZKK58ER"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyCNZVuK1AB2J9GARY92Tk_fib0lrflnuAQ",
+  authDomain: "pc-builder-a804d.firebaseapp.com",
+  projectId: "pc-builder-a804d",
+  storageBucket: "pc-builder-a804d.appspot.com",
+  messagingSenderId: "42029449411",
+  appId: "1:42029449411:web:b12adc8178dbe8f4c62276",
+  measurementId: "G-1NSZKK58ER"
+};
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// export const auth = getAuth(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth }
