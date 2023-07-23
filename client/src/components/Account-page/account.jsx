6ff00173fe5ca2811eb678c57d30f1f54b971658
Noah8863 <stacknoah99@gmail.com>
2023-07-23 13:19:28 -0600

    successfully have the data being passed through

--- client/src/components/Account-page/account.jsx
+++ client/src/components/Account-page/account.jsx
@@ -1,25 +1,37 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import TestImage from "../../images/custom-PC-3.jpg";
 
 function AccountComponent() {
   const location = useLocation();
   const userData = location.state;
   return (
-    <div className="containermin-w-full mx-auto bg-blue-400 p-8 m-4">
-        <p className="text-2xl p-4">My Account</p>
-      <div className="grid grid-cols-2 ">
-        <div className="bg-green-400 w-5/6">First container</div>
-        <div className="bg-pink-400">Second container</div>
+    <div className="container xl:w-3/4 lg:w-full md:w-full sm:w-full mx-auto bg-blue-400 p-8 m-4">
+      <p className="text-2xl p-2 text-center">My Account</p>
+      <div className="grid grid-cols-3 gap-6">
+        <div className="bg-green-400 h-full grid col-span-1 justify-left p-4 m-4 ">
+          <img
+            className="w-2/3 m-auto"
+            src={TestImage}
+            alt="profilePicture"
+          ></img>
+          <p className="text-xl p-4">Welcome: {userData.firstName} </p>
+        </div>
+        <div className="bg-pink-400 col-span-2 h-full p-4 m-4">
+          <p className="text-2xl p-2 text-left">
+            {userData.firstName} {userData.lastName}
+          </p>
+          <div className="border-t border-gray-500"></div>
+          <div className="text-blue-600 text-lg flex">
+            Email: <p className="text-black pl-2">{userData.email}</p>
+          </div>
+          <div className="text-blue-600 text-lg flex">
+            Password: <p className="text-black pl-2">{userData.password}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
 
 export default AccountComponent;
-
-{
-  /* <p>This is the email: {userData.email}</p>
-      <p>This is the password: {userData.password}</p>
-      <p>This is the First Name: {userData.firstName}</p>
-      <p>This is the Last Name: {userData.lastName}</p> */
-}
