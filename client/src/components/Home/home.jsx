ded8ea4fa93ad54e350c459d604c077f4ee249f9
Noah8863 <stacknoah99@gmail.com>
2023-08-02 23:26:51 -0600

    updated home page with a different UI/style

--- client/src/components/Home/home.jsx
+++ client/src/components/Home/home.jsx
@@ -218,39 +218,39 @@ function Home() {
           </div>
         </div>
         {/* Section 3 */}
-        <div>
-          <p className="text-2xl text-black text-center">
-            Check out these hot deals across all categories!
-          </p>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 bg-customLightBlue grab-3">
-            {Object.entries(partCategorie).map(([category, prefixes]) => {
-              const cheapestItem = findCheapestItem(prefixes);
-              return (
-                <div
-                  key={category}
-                  className="grid">
-                  <div className="grid p-6 bg-white m-4 items-center text-center text-xl justify-center">
-                    <img
-                      src={cheapestItem.img}
-                      alt={cheapestItem.title}
-                      className="lg:w-60 sm:w-full z-1 justify-center flex"
-                    />
-                  
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 bg-customLightBlue grab-3">
+          <div className="col-span-2 p-6 bg-customLightBlue">
+            <p className="text-2xl text-black text-center">
+              Check out these hot deals across all categories!
+            </p>
+          </div>
+          {Object.entries(partCategorie).map(([category, prefixes]) => {
+            const cheapestItem = findCheapestItem(prefixes);
+            return (
+              <div key={category} className="grid">
+                <div className="grid p-6 bg-white m-4 items-center text-center text-xl justify-center">
+                  <img
+                    src={cheapestItem.img}
+                    alt={cheapestItem.title}
+                    className="lg:w-60 sm:w-full z-1 justify-center flex"
+                  />
+
                   <div className="text-small h-auto">
                     <p className="text-sm underline">{cheapestItem.title}</p>
                     <p className="text-lg">Price: {cheapestItem.price}</p>
                   </div>
-                  </div>
                 </div>
-              );
-            })}
-          </div>
+              </div>
+            );
+          })}
         </div>
 
-        <div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 bg-customLightBlue grab-3">
+          <div className="col-span-2 p-6 bg-customLightBlue">
           <p className="text-2xl text-black text-center">
             Looking to maximize the preformance of your PC? We got you covered!
           </p>
+          </div>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 bg-customLightBlue grab-3">
             {Object.entries(partCategories).map(([category, prefixes]) => {
               const mostExpensiveItem = findMostExpensiveItem(prefixes);
