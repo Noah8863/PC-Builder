bb742c19bd0e97262f804fe6f4a0bc067e719ce8
Noah8863 <stacknoah99@gmail.com>
2023-08-02 22:18:44 -0600

    added a better UI for account page

--- client/src/components/Account-page/account.jsx
+++ client/src/components/Account-page/account.jsx
@@ -1,39 +1,61 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import { auth } from "../../config/firebase";
+import { onAuthStateChanged } from "firebase/auth";
 import TestImage from "../../images/custom-PC-3.jpg";
 
 function AccountComponent() {
   const location = useLocation();
   const userData = location.state;
   const [showPassword, setShowPassword] = useState(false);
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setCurrentUser(user);
+      } else {
+        setCurrentUser(null);
+      }
+    });
+  
+    // Cleanup the listener when the component unmounts
+    return () => unsubscribe();
+  }, []);
+
+
   return (
     <div className="container xl:w-3/4 lg:w-full md:w-full sm:w-full mx-auto bg-blue-400 p-8 m-4">
       <p className="text-2xl p-2 text-center">My Account</p>
       <div className="grid grid-cols-3 gap-6">
         <div className="bg-green-400 h-full grid col-span-1 justify-left p-4 m-4 ">
           <img className="w-2/3" src={TestImage} alt="profilePicture"></img>
-          <p className="text-xl p-4">Welcome, {userData.firstName} </p>
+          {currentUser ? (
+            <p className="text-xl p-4">Welcome, {currentUser.firstName}</p>
+          ) : (
+            <p className="text-xl p-4">Welcome, Guest</p>
+          )}
         </div>
-        <div className="bg-pink-400 col-span-2 h-full p-4 m-4">
-          <p className="text-2xl py-2 text-left">
-            {userData.firstName} {userData.lastName}
-          </p>
-          <div className="border-t border-gray-500 py-2"></div>
-          <div className="text-blue-600 text-lg flex">
-            Email: <p className="text-black pl-2">{userData.email}</p>
+        {currentUser ? (
+          <div className="bg-pink-400 col-span-2 h-full p-4 m-4">
+            <p className="text-2xl py-2 text-left">
+              {currentUser.displayName}
+            </p>
+            <div className="border-t border-gray-500 py-2"></div>
+            <div className="text-blue-600 text-lg flex">
+              Email: <p className="text-black pl-2">{currentUser.email}</p>
+            </div>
+            {/* Display other user data here */}
+            <button onClick={() => setShowPassword(!showPassword)}>
+              {showPassword ? "Hide Password" : "Show Password"}
+            </button>
           </div>
-          <div className="text-blue-600 text-lg flex">
-            Password:{" "}
-            {showPassword ? (
-              <p className="text-black pl-2">{userData.password}</p>
-            ) : (
-              <p className="text-black pl-2">*****</p>
-            )}
+        ) : (
+          <div className="bg-pink-400 col-span-2 h-full p-4 m-4">
+            {/* Show a message or redirect to the login page if no user is signed in */}
+            <p className="text-xl py-2 text-left">Please sign in to view your account</p>
           </div>
-          <button onClick={() => setShowPassword(!showPassword)}>
-            {showPassword ? "Hide Password" : "Show Password"}
-          </button>
-        </div>
+        )}
       </div>
     </div>
   );
