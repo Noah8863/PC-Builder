b7e17076c6f766b5faa8d76719d22de2c3c9f3b2
Noah Hoffman <stacknoah99@gmail.com>
2023-08-09 13:28:51 -0600

    started basic layout for profile

--- client/src/components/Account-page/account.jsx
+++ client/src/components/Account-page/account.jsx
@@ -3,6 +3,9 @@ import { useLocation } from "react-router-dom";
 import { auth } from "../../config/firebase";
 import { onAuthStateChanged } from "firebase/auth";
 import TestImage from "../../images/custom-PC-3.jpg";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import ConstructionIcon from '@mui/icons-material/Construction';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 function AccountComponent() {
   const location = useLocation();
@@ -18,11 +21,10 @@ function AccountComponent() {
         setCurrentUser(null);
       }
     });
-  
+
     // Cleanup the listener when the component unmounts
     return () => unsubscribe();
   }, []);
-
 
   return (
     <div className="container xl:w-3/4 lg:w-full md:w-full sm:w-full mx-auto bg-blue-400 p-8 m-4">
@@ -31,16 +33,27 @@ function AccountComponent() {
         <div className="bg-green-400 h-full grid col-span-1 justify-left p-4 m-4 ">
           <img className="w-2/3" src={TestImage} alt="profilePicture"></img>
           {currentUser ? (
-            <p className="text-xl p-4">Welcome, {currentUser.firstName}</p>
+            <div>
+              <p className="text-xl p-4">Welcome, {currentUser.firstName}</p>
+              <div className="space-y-2 bg-blue-400">
+                <button className="block w-full flex items-center justify-between">
+                  <span className="ml-2">Profile <AccountCircleIcon /> </span>
+                </button>
+                <button className="block w-full flex items-center justify-between">
+                  <span className="ml-2">Builds <ConstructionIcon /> </span>
+                </button>
+                <button className="block w-full flex items-center justify-between">
+                  <span className="ml-2">Builds <LogoutIcon /> </span>
+                </button>
+              </div>
+            </div>
           ) : (
             <p className="text-xl p-4">Welcome, Guest</p>
           )}
         </div>
         {currentUser ? (
           <div className="bg-pink-400 col-span-2 h-full p-4 m-4">
-            <p className="text-2xl py-2 text-left">
-              {currentUser.displayName}
-            </p>
+            <p className="text-2xl py-2 text-left">{currentUser.displayName}</p>
             <div className="border-t border-gray-500 py-2"></div>
             <div className="text-blue-600 text-lg flex">
               Email: <p className="text-black pl-2">{currentUser.email}</p>
@@ -53,7 +66,9 @@ function AccountComponent() {
         ) : (
           <div className="bg-pink-400 col-span-2 h-full p-4 m-4">
             {/* Show a message or redirect to the login page if no user is signed in */}
-            <p className="text-xl py-2 text-left">Please sign in to view your account</p>
+            <p className="text-xl py-2 text-left">
+              Please sign in to view your account
+            </p>
           </div>
         )}
       </div>
